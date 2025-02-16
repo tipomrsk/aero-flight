8616ff25b2f3918a8e75d7e6fb614d248ea2dbda
Mateus Bougleux <mateusbougleuxmaia@hotmail.com>
2025-02-16 11:17:54 -0300

    Adiciona testes para filtragem de viagens em OrderTravelController, incluindo validações para parâmetros de data e status, além de garantir respostas corretas para diferentes cenários de filtragem.

---
+++ tests/Feature/OrderTravel/GetFilteredOrderTravelTest.php
@@ -0,0 +1,193 @@
+<?php
+
+beforeEach(function () {
+    $this->token = login();
+    $this->withHeader('Authorization', "Bearer {$this->token}");
+    $this->withHeader('Accept', "application/json");
+
+    $this->orderTravel = createOrderTravel();
+});
+
+it('should not list order travels without filters', function () {
+    $response = $this->get('/api/order-travel');
+
+    $response->assertStatus(422)
+        ->assertJsonValidationErrors(['start_at', 'status']);
+});
+
+it('should list order travels with start_at and status', function () {
+    // Os filtros devem ser enviados como um query string
+
+    $filter = [
+        'start_at' => $this->orderTravel->start_date,
+        'status' => $this->orderTravel->status,
+    ];
+
+    $response = $this->get('/api/order-travel?' . http_build_query($filter));
+
+    $response->assertStatus(200)
+        ->assertJson([
+            [
+                'uuid' => $this->orderTravel->uuid,
+                'origin' => $this->orderTravel->origin,
+                'destination' => $this->orderTravel->destination,
+                'start_date' => $this->orderTravel->start_date,
+                'end_date' => $this->orderTravel->end_date,
+                'status' => $this->orderTravel->status,
+            ],
+        ]);
+});
+
+it('should not list order travels with invalid status', function () {
+    $filter = [
+        'start_at' => $this->orderTravel->start_date,
+        'status' => 'invalidStatus',
+    ];
+
+    $response = $this->get('/api/order-travel?' . http_build_query($filter));
+
+    $response->assertStatus(422)
+        ->assertJsonValidationErrors(['status']);
+});
+
+it('should not list order travels with invalid start_at', function () {
+    $filter = [
+        'start_at' => 'invalidDate',
+        'status' => $this->orderTravel->status,
+    ];
+
+    $response = $this->get('/api/order-travel?' . http_build_query($filter));
+
+    $response->assertStatus(422)
+        ->assertJsonValidationErrors(['start_at']);
+});
+
+it('should not list order travels with start_at bigger than end_at', function () {
+    $filter = [
+        'start_at' => $this->orderTravel->start_at,
+        'end_at' => '2024-04-01',
+        'status' => $this->orderTravel->status,
+    ];
+
+    $response = $this->get('/api/order-travel?' . http_build_query($filter));
+
+    $response->assertStatus(422)
+        ->assertJsonValidationErrors(['start_at']);
+});
+
+it('should list order travels with start_at, end_at and status', function () {
+    $filter = [
+        'start_at' => $this->orderTravel->start_date,
+        'end_at' => date('Y-m-d', strtotime($this->orderTravel->start_date . ' + 1 week')),
+        'status' => $this->orderTravel->status,
+    ];
+
+    $response = $this->get('/api/order-travel?' . http_build_query($filter));
+
+    $response->assertStatus(200)
+        ->assertJson([
+            [
+                'uuid' => $this->orderTravel->uuid,
+                'origin' => $this->orderTravel->origin,
+                'destination' => $this->orderTravel->destination,
+                'start_date' => $this->orderTravel->start_date,
+                'end_date' => $this->orderTravel->end_date,
+                'status' => $this->orderTravel->status,
+            ],
+        ]);
+});
+
+it('should list order travels with start_at, end_at, status and destiny', function () {
+    $filter = [
+        'start_at' => $this->orderTravel->start_date,
+        'end_at' => date('Y-m-d', strtotime($this->orderTravel->start_date . ' + 1 week')),
+        'status' => $this->orderTravel->status,
+        'destiny' => $this->orderTravel->destination,
+    ];
+
+    $response = $this->get('/api/order-travel?' . http_build_query($filter));
+
+    $response->assertStatus(200)
+        ->assertJson([
+            [
+                'uuid' => $this->orderTravel->uuid,
+                'origin' => $this->orderTravel->origin,
+                'destination' => $this->orderTravel->destination,
+                'start_date' => $this->orderTravel->start_date,
+                'end_date' => $this->orderTravel->end_date,
+                'status' => $this->orderTravel->status,
+            ],
+        ]);
+});
+
+it('should not list order travels with invalid destiny', function () {
+    $filter = [
+        'start_at' => $this->orderTravel->start_date,
+        'end_at' => date('Y-m-d', strtotime($this->orderTravel->start_date . ' + 1 week')),
+        'status' => $this->orderTravel->status,
+        'destiny' => 'invalidDestiny',
+    ];
+
+    $response = $this->get('/api/order-travel?' . http_build_query($filter));
+
+    $response->assertStatus(200)
+        ->assertJson([]);
+});
+
+it('should not list order travels with invalid start_at and end_at', function () {
+    $filter = [
+        'start_at' => 'invalidDate',
+        'end_at' => 'invalidDate',
+        'status' => $this->orderTravel->status,
+    ];
+
+    $response = $this->get('/api/order-travel?' . http_build_query($filter));
+
+    $response->assertStatus(422)
+        ->assertJsonValidationErrors(['start_at', 'end_at']);
+});
+
+it('should return a list of order travels with a part of destination, start_at and status', function () {
+    $filter = [
+        'start_at' => $this->orderTravel->start_date,
+        'status' => $this->orderTravel->status,
+        'destiny' => substr($this->orderTravel->destination, 0, 5),
+    ];
+
+    $response = $this->get('/api/order-travel?' . http_build_query($filter));
+
+    $response->assertStatus(200)
+        ->assertJson([
+            [
+                'uuid' => $this->orderTravel->uuid,
+                'origin' => $this->orderTravel->origin,
+                'destination' => $this->orderTravel->destination,
+                'start_date' => $this->orderTravel->start_date,
+                'end_date' => $this->orderTravel->end_date,
+                'status' => $this->orderTravel->status,
+            ],
+        ]);
+});
+
+it('should return a list of order travels with a part of destination, start_at, end_at and status', function () {
+    $filter = [
+        'start_at' => $this->orderTravel->start_date,
+        'end_at' => date('Y-m-d', strtotime($this->orderTravel->start_date . ' + 1 week')),
+        'status' => $this->orderTravel->status,
+        'destiny' => substr($this->orderTravel->destination, 0, 5),
+    ];
+
+    $response = $this->get('/api/order-travel?' . http_build_query($filter));
+
+    $response->assertStatus(200)
+        ->assertJson([
+            [
+                'uuid' => $this->orderTravel->uuid,
+                'origin' => $this->orderTravel->origin,
+                'destination' => $this->orderTravel->destination,
+                'start_date' => $this->orderTravel->start_date,
+                'end_date' => $this->orderTravel->end_date,
+                'status' => $this->orderTravel->status,
+            ],
+        ]);
+});
