d22d3dc794521810aa59f1500e013ff8d2a4d2a5
Mateus Bougleux <mateusbougleuxmaia@hotmail.com>
2025-02-16 23:14:13 -0300

    reafact!: use controller() on Route

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,12 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function (): void {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::prefix('order-travel')->group(function (): void {
-        Route::get('/', [OrderTravelController::class, 'getAll']);
-        Route::post('/', [OrderTravelController::class, 'store']);
-        Route::get('/{orderTravel}', [OrderTravelController::class, 'show']);
-        Route::put('/{orderTravel}', [OrderTravelController::class, 'update']);
-        Route::put('/update-status/{orderTravel}', [OrderTravelController::class, 'updateStatus']);
-        Route::delete('/{orderTravel}', [OrderTravelController::class, 'destroy']);
+    Route::controller(OrderTravelController::class)->prefix('order-travel')->group(function (): void {
+        Route::get('/', 'getAll');
+        Route::post('/', 'store');
+        Route::get('/{orderTravel}', 'show');
+        Route::put('/{orderTravel}', 'update');
+        Route::put('/update-status/{orderTravel}', 'updateStatus');
+        Route::delete('/{orderTravel}', 'destroy');
     });
 });
